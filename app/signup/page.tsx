0741bd6f83fc72b2e79495c4b95b015e0e260ac1
GITZMBE <lucasfilip@hotmail.se>
2024-04-10 11:43:00 +0200

    Initialize a sign up page

---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <main className="w-full min-h-screen flex justify-center items-center bg-primary">
+      
+    </main>
+  )
+}
+
+export default SignupPage
