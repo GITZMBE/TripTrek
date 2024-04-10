461ba65b9240f1d2cde5e7bba0ac22456002a0c4
GITZMBE <lucasfilip@hotmail.se>
2024-04-10 11:38:34 +0200

    Update start page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen flex justify-center items-center bg-primary">
-      <h1 className="text-6xl font-bold tracking-[8px]">Home</h1>
+      <Image src='/logo.png' width='550' height='200' alt='' />
     </main>
   );
 }
