61ea794be1234d17392b02e1c270795733b9759e
GITZMBE <lucasfilip@hotmail.se>
2024-04-10 11:04:26 +0200

    Export files through index file in components folder and ui folder

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Navbar from "./Navbar";
+import Pathbar from "../Pathbar";
 
 const Header = () => {
   return (
-    <header className='w-full py-4 px-4 sm:px-8 md:px-12'>Header</header>
-  )
-}
+    <header className='fixed top-0 flex flex-col gap-2 bg-secondary w-full py-4 px-4 sm:px-8 md:px-12 shadow-md'>
+      <div className="flex justify-between items-center">
+        <Image src='/logo.png' width='180' height='0' alt='' />
+        <Navbar />  
+      </div>
+      <Pathbar />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
