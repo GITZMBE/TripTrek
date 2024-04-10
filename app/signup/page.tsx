bb3d3656b5e40a256c11226b379e522460f4993e
GITZMBE <lucasfilip@hotmail.se>
2024-04-10 13:17:42 +0200

    Use SignupForm in SignupPage

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { SignupForm } from "@/src/components/forms";
+import React from "react";
 
 const SignupPage = () => {
   return (
-    <main className="w-full min-h-screen flex justify-center items-center bg-primary">
-      
+    <main className='w-full min-h-screen flex justify-center items-center bg-primary'>
+      <SignupForm />
     </main>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;
