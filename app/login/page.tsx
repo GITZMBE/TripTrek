c874045bebaad8d3cde0adc0068bb19e96b9250f
GITZMBE <lucasfilip@hotmail.se>
2024-04-11 10:18:45 +0200

    Create LoginPage

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import { LoginForm } from '@/src/components/forms'
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <main className='w-full min-h-screen flex flex-col justify-center items-center gap-8 bg-primary'>
+      <h1 className="text-5xl font-semibold"><span className="text-accent">Log</span>in</h1>
+      <LoginForm />
+    </main>
+  )
+}
+
+export default LoginPage
