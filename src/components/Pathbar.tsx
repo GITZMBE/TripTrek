b51f07307c8a515c235b623c260f6226366e1487
GITZMBE <lucasfilip@hotmail.se>
2024-04-11 10:36:32 +0200

    Update Pathbar to handle which pages are allowed when you are not logged in

--- src/components/Pathbar.tsx
+++ src/components/Pathbar.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useRecoilValue } from "recoil";
+import { loggedInUserState } from "../recoil";
 
 const Pathbar = () => {
+  const router = useRouter();
   const pathName = usePathname();
   let paths = pathName.split("/");
   paths.shift();
   const currentPath = paths.findLast((_) => true);
+  const user_token = useRecoilValue(loggedInUserState);
+
+  useEffect(() => {
+    const isLoggedIn = user_token !== null;
+
+    if (!isLoggedIn && pathName !== '/login' && pathName !== '/signup') {
+      router.push('/login');
+    }
+  }, [pathName, user_token]);
 
   return (
     <div className='flex gap-2'>
@@ -29,7 +41,7 @@ const Pathbar = () => {
             <Link
               key={i}
               href={subpath}
-              className='text-secondary hover:text-light'
+              className='text-grey hover:text-light'
             >
               /{path}
             </Link>
