b500d3a9e1c01678a765fd3167db434e0c22eb4d
GITZMBE <lucasfilip@hotmail.se>
2024-04-15 14:31:36 +0200

    Fix endpoint to change user information

--- app/api/users/update/[id]/route.ts
+++ app/api/users/update/[id]/route.ts
@@ -1,7 +1,6 @@
 import prisma from '@/prisma';
-import { put } from '@vercel/blob';
 import bcrypt from 'bcrypt';
-import imageToBase64, {} from 'image-to-base64';
+import { NextResponse } from 'next/server';
 
 export const PUT = async (req: Request, context: any) => {
   const { params } = context;
@@ -9,7 +8,6 @@ export const PUT = async (req: Request, context: any) => {
 
   try {
     const { name, email, password, avatar } = await req.json();
-    console.log({name, email, password, avatar})
 
     const dataToUpdate: any = {};
     if (name && name.trim() !== '') {
@@ -22,14 +20,9 @@ export const PUT = async (req: Request, context: any) => {
       const hashedPassword = bcrypt.hashSync(password, 10);
       dataToUpdate.hashedPassword = hashedPassword;
     }
-    if (avatar && avatar.trim() !== '') {
-      // const blob = await put(avatar.name, avatar);
-      // console.log(blob)
-      // dataToUpdate.avatar = blob.url;
+    if (avatar) {
       dataToUpdate.avatar = avatar
     }
-
-    console.log(dataToUpdate)
 
     const updatedUser = await prisma.user.update({
       where: {
@@ -38,8 +31,8 @@ export const PUT = async (req: Request, context: any) => {
       data: dataToUpdate
     });
 
-    return new Response(JSON.stringify(updatedUser), { status: 200 });
-  } catch (error) {
-    return new Response(JSON.stringify({ message: "Could not update user information" }), { status: 500 });
+    return NextResponse.json(updatedUser);
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message });
   }
 }
