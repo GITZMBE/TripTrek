d5f494fce49c98a38d289b38c2f95e1e3284b8b2
GITZMBE <lucasfilip@hotmail.se>
2024-04-15 13:42:46 +0200

    Fix code in UserPage

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,43 +1,69 @@
-'use client';
+"use client";
 
-import { FormButton } from '@/src/components/forms/ui';
-import { FormInput } from '@/src/components/ui';
-import { loggedInUserState } from '@/src/recoil';
-import React, { useEffect, useState } from 'react'
-import { useRecoilState } from 'recoil';
-import bcrypt from 'bcrypt';
-import Image from 'next/image';
-import FormFileInput from '@/src/components/forms/ui/FormFileInput';
-import { put } from '@vercel/blob';
+import { FormButton } from "@/src/components/forms/ui";
+import { FormInput } from "@/src/components/ui";
+import { loggedInUserState } from "@/src/recoil";
+import React, { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import bcrypt from "bcrypt";
+import Image from "next/image";
+import FormFileInput from "@/src/components/forms/ui/FormFileInput";
+import { useErrorMessage } from "@/src/hooks";
 
 const UserPage = ({ params }: { params: { id: string } }) => {
   const [user_token, setUserToken] = useRecoilState(loggedInUserState);
-  const [ updateable, setUpdateable ] = useState(false);
-  const [ formData, setFormData ] = useState<{ name: string, email: string, password: string, avatar: any }>({ name: '', email: '', password: '', avatar: '' });
+  const { errorMessage, setErrorMessage } = useErrorMessage();
+  const [updateable, setUpdateable] = useState(false);
+  const [formData, setFormData] = useState<{
+    name: string;
+    email: string;
+    password: string;
+    avatar: any;
+  }>({ name: "", email: "", password: "", avatar: "" });
 
-  const update = async () => {
-    const { url } = await fetch(process.env.NEXT_PUBLIC_BASEURL + '/api/upload/image', { 
-      method: 'POST', 
-      headers: { 'content-types': formData.avatar.type || 'application/octet-stream' }, 
-      body: JSON.stringify(formData.avatar)
-    });
-    console.log(url)
-    setFormData({ ...formData, avatar: url });
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/users/update/${params.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Types': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    });
+  const uploadImage = async () => {
+    const imageData = await fetch(
+      process.env.NEXT_PUBLIC_BASEURL + "/api/upload/image",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": formData.avatar.type,
+        },
+        body: formData.avatar,
+      }
+    );
+    const savedImage = await imageData.json();
+    if (savedImage.message) {
+      return savedImage;
+    }
+    setFormData({ ...formData, avatar: savedImage.url });
+  };
+
+  const updateUserInfo = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASEURL}/api/users/update/${params.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
     const updatedUser = await res.json();
     return updatedUser;
   };
 
   const handleUpdate = async () => {
-    const newUser = await update();
-    if (newUser.message) {
-      console.log(newUser.message);
+    const uploadedImage = await uploadImage();
+    if (uploadedImage && uploadedImage.message) {
+      setErrorMessage(uploadedImage.message);
+      return;
+    }
+
+    const newUser = await updateUserInfo();
+    if (newUser && newUser.message) {
+      setErrorMessage(newUser.message);
       return;
     }
 
@@ -46,8 +72,13 @@ const UserPage = ({ params }: { params: { id: string } }) => {
 
   useEffect(() => {
     let samePassword = false;
-    
-    if ((formData.name !== user_token?.user.name && formData.name !== '') || (formData.email !== user_token?.user.email && formData.email !== '') || (!samePassword && formData.password !== '') || (formData.avatar !== user_token?.user.avatar && formData.avatar !== null)) {
+
+    if (
+      (formData.name !== user_token?.user.name && formData.name !== "") ||
+      (formData.email !== user_token?.user.email && formData.email !== "") ||
+      (!samePassword && formData.password !== "") ||
+      (formData.avatar !== user_token?.user.avatar && formData.avatar !== null)
+    ) {
       setUpdateable(true);
       return;
     }
@@ -55,23 +86,58 @@ const UserPage = ({ params }: { params: { id: string } }) => {
   }, [formData]);
 
   return (
-    <main className='w-full min-h-screen flex flex-col justify-center items-center gap-8 bg-primary'>
-      <div className='flex gap-8 w-1/2'>
+    <main className='w-full min-h-screen flex flex-col justify-center items-center gap-8 bg-primary py-24 px-4 sm-px-8 md-px-12'>
+      <div className='flex justify-center items-center flex-col sm:flex-row gap-8 w-full md:w-3/4 xl:w-1/2'>
         {user_token !== null && user_token.user.avatar !== null ? (
-          <Image src={user_token.user.avatar} width='256' height='256' alt="Profile Picture" />
+          <Image
+            src={user_token.user.avatar}
+            width='256'
+            height='256'
+            alt='Profile Picture'
+          />
         ) : (
-          <Image src='/male_default_avatar.png' width='256' height='256' alt='' />
+          <Image
+            src='/male_default_avatar.png'
+            width='256'
+            height='256'
+            alt=''
+          />
         )}
         <div className='w-full h-full flex flex-col items-center gap-4'>
-          <FormInput name='name' placeholder='Name' value={formData} setValue={setFormData} />
-          <FormInput name='email' placeholder='Email' value={formData} setValue={setFormData} />
-          <FormInput name='password' placeholder='Password' value={formData} setValue={setFormData} />
-          <FormFileInput name='avatar' value={formData} setValue={setFormData} />
-          <FormButton label='Update' onClick={handleUpdate} outline={!updateable} disabled={!updateable} />
+          <FormInput
+            name='name'
+            placeholder='Name'
+            value={formData}
+            setValue={setFormData}
+          />
+          <FormInput
+            name='email'
+            placeholder='Email'
+            value={formData}
+            setValue={setFormData}
+          />
+          <FormInput
+            name='password'
+            placeholder='Password'
+            value={formData}
+            setValue={setFormData}
+          />
+          <FormFileInput
+            name='avatar'
+            value={formData}
+            setValue={setFormData}
+          />
+          <p className='text-error'>{errorMessage}</p>
+          <FormButton
+            label='Update'
+            onClick={handleUpdate}
+            outline={!updateable}
+            disabled={!updateable}
+          />
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
