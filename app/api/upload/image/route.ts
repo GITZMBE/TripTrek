b9d3fd53c2df00805b0948f6b10ce57361ec6b2e
GITZMBE <lucasfilip@hotmail.se>
2024-04-15 13:40:29 +0200

    Install nanoid

--- app/api/upload/image/route.ts
+++ app/api/upload/image/route.ts
@@ -1,8 +1,25 @@
 import { put } from "@vercel/blob";
+import { customAlphabet } from "nanoid";
+import { NextResponse } from "next/server";
+
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  7
+);
 
 export const POST = async (req: Request, res: Response) => {
-  const body = await req.json();
-  const upload = await put(body.name, body, { access: 'public' });
+  try {
+    if (!req.body) return;
+    const file = req.body || '';
+    const contentType = req.headers.get("Content-Type") || "text/plain";
+    const filename = `${nanoid()}.${contentType.split('/')[1]}`;
+    const upload = await put(filename, file, {
+      contentType,
+      access: "public",
+    });
 
-  return new Response(JSON.stringify(upload));
+    return NextResponse.json({ url: upload.url });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message });
+  }
 };
