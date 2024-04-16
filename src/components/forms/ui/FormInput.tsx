d3f39b14d42a1eb64e870bb4fcd43bced87cc7a1
GITZMBE <lucasfilip@hotmail.se>
2024-04-16 12:43:34 +0200

    Change form ui components to match react hook form functionality

--- src/components/forms/ui/FormInput.tsx
+++ src/components/forms/ui/FormInput.tsx
@@ -1,48 +1,49 @@
 "use client";
 
-import React, { SetStateAction, useEffect, useState } from "react";
+import React, { SetStateAction, useState } from "react";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
 import { IconType } from "react-icons";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 
 interface InputProps {
-  icon?: IconType
+  icon?: IconType;
   type?: string;
   name: string;
   placeholder?: string;
-  required?: boolean;
-  value: any;
-  setValue: SetStateAction<any>;
+  required?: boolean | string;
+  // value: any;
+  // setValue: SetStateAction<any>;
+  register: UseFormRegister<any>;
+  errors?: FieldErrors<any>;
 }
 
 export const FormInput = ({
   icon: Icon,
-  type = 'text',
+  type = "text",
   name,
   placeholder = "",
   required = false,
-  value,
-  setValue,
+  register,
+  errors
 }: InputProps) => {
   const [inputType, setInputType] = useState<string>(type);
 
   return (
-    <div
-      className={`relative flex justify-end items-center w-full cursor-default ${
-        Icon !== undefined ? '' : ''
-      }`}
-    >
-      <input
-        type={inputType}
-        name={name}
-        placeholder={placeholder}
-        required={required}
-        onChange={(e) => setValue({ ...value, [e.target.name]: e.target.value})}
-        value={value[name]}
-        className='w-full px-2 py-1 rounded-full transition-all duration-300 bg-light text-secondary border-2 border-grey/50 focus:border-grey outline-none'
-      />
-      {
-        type.toLowerCase() === "password" ? (
+    <div className="flex flex-col">
+      <div
+        className={`relative flex justify-end items-center w-full cursor-default ${
+          Icon !== undefined ? "" : ""
+        }`}
+      >
+        <input
+          {...register(name, { required: required })}
+          type={inputType}
+          name={name}
+          placeholder={placeholder}
+          className='w-full px-2 py-1 rounded-full transition-all duration-300 bg-light text-secondary border-2 border-grey/50 focus:border-grey outline-none'
+        />
+        {type.toLowerCase() === "password" ? (
           inputType.toLowerCase() === "password" ? (
             <FaEye
               size={18}
@@ -56,9 +57,16 @@ export const FormInput = ({
               onClick={(_) => setInputType("password")}
             />
           )
-        ) : Icon !== undefined &&(
-          <Icon size={32} className='absolute text-center p-2 rounded-[16px] text-grey hover:text-primary bg-white cursor-pointer transition-all duration-300' />
+        ) : (
+          Icon !== undefined && (
+            <Icon
+              size={32}
+              className='absolute text-center p-2 rounded-[16px] text-grey hover:text-primary bg-white cursor-pointer transition-all duration-300'
+            />
+          )
         )}
+      </div>
+      {errors?.[name] && <p className='text-error'>{errors?.[name]?.message as string}</p>}
     </div>
   );
 };
