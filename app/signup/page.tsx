cb989efa665d5cadf4094dc12da75a0b50f0b56c
GITZMBE <lucasfilip@hotmail.se>
2024-04-16 10:41:37 +0200

    Add some padding to auth form pages

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const SignupPage = () => {
   return (
-    <main className='w-full min-h-screen flex flex-col justify-center items-center gap-8 bg-primary'>
+    <main className='w-full min-h-screen flex flex-col justify-center items-center gap-8 pt-20 bg-primary'>
       <h1 className="text-5xl font-semibold"><span className="text-accent">Sign</span>up</h1>
       <SignupForm />
     </main>
