2ca5507bc92b3c21a101fcbf1369b80a1fc914bb
GITZMBE <lucasfilip@hotmail.se>
2024-04-16 10:41:37 +0200

    Add some padding to auth form pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <main className='w-full min-h-screen flex flex-col justify-center items-center gap-8 bg-primary'>
+    <main className='w-full min-h-screen flex flex-col justify-center items-center gap-8 pt-20 bg-primary'>
       <h1 className="text-5xl font-semibold"><span className="text-accent">Log</span>in</h1>
       <LoginForm />
     </main>
   )
 }
 
-export default LoginPage
+export default LoginPage;
