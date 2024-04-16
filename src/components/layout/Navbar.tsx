e594b0cd5966da10f92f9368103fbb8704560254
GITZMBE <lucasfilip@hotmail.se>
2024-04-16 10:48:38 +0200

    Change style to Navbar and add a dropdown menu as well as add NavLinks

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,37 +3,80 @@
 import { loggedInUserState } from "@/src/recoil";
 import Link from "next/link";
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { RxAvatar } from "react-icons/rx";
 import { useRecoilState } from "recoil";
+import { NavLink } from "./ui";
+import { useToggleMenu } from "@/src/hooks";
 
 const Navbar = () => {
   const [user_token, setUserToken] = useRecoilState(loggedInUserState);
+  const { isOpen, setIsOpen, navComponent } = useToggleMenu();
+
+  const handleToggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <nav className='flex gap-4 items-center'>
-      {!user_token ? (
-        <>
-          <Link href='/login'>Login</Link>
-          <Link href='/signup'>Signup</Link>
-        </>
-      ) : (
-        <>
-          <Link href={`/users/${user_token.user.id}`} className='flex gap-4 items-center'>
-            {user_token.user.avatar ? (
-              <img
-                src={user_token.user.avatar || ""}
-                className='w-8 aspect-square rounded-full'
-                alt=''
-              />
-            ) : (
-              <RxAvatar size={32} className='text-grey' />
-            )}
+    <nav className='relative flex gap-4 items-center' ref={navComponent}>
+      {user_token && (
+        <Link
+          href={`/users/${user_token.user.id}`}
+          className='flex gap-4 items-center'
+        >
+          {user_token.user.avatar ? (
+            <img
+              src={user_token.user.avatar || ""}
+              className='w-8 aspect-square rounded-full'
+              alt=''
+            />
+          ) : (
+            <RxAvatar size={32} className='text-grey' />
+          )}
 
-            <p>{user_token.user.name || user_token.user.email}</p>
-          </Link>
-          <button onClick={(_) => setUserToken(null)}>Log out</button>
-        </>
+          <p>{user_token.user.name || user_token.user.email}</p>
+        </Link>
       )}
+      <GiHamburgerMenu
+        size={24}
+        className='text-light hover:text-white cursor-pointer'
+        onClick={handleToggleMenu}
+      />
+      <div
+        className={`absolute top-12 right-0 flex flex-col min-w-52 overflow-hidden rounded-lg shadow-lg shadow-primary bg-secondary ${
+          !isOpen && "hidden"
+        }`}
+      >
+        {!user_token ? (
+          <>
+            <NavLink
+              href='/login'
+              label='Login'
+              onClick={() => setIsOpen(false)}
+            />
+            <NavLink
+              href='/signup'
+              label='Signup'
+              onClick={() => setIsOpen(false)}
+            />
+          </>
+        ) : (
+          <>
+            <NavLink
+              href={`/users/${user_token.user.id}/favorites`}
+              label='Favorite landings'
+              onClick={() => setIsOpen(false)}
+            />
+            <hr className='border-primary border-thin mx-4' />
+            <button
+              onClick={(_) => setUserToken(null)}
+              className='text-left px-4 py-2 bg-secondary hover:bg-primary transition'
+            >
+              Log out
+            </button>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
