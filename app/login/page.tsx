662f9e42fdc6cef1e8d48d31ef360e7ec375fb6b
GITZMBE <lucasfilip@hotmail.se>
2024-04-17 20:43:21 +0200

    Change Container attributes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,13 @@
 import { LoginForm } from '@/src/components/forms'
+import { Container } from '@/src/components/layout';
 import React from 'react'
 
 const LoginPage = () => {
   return (
-    <main className='w-full min-h-screen flex flex-col justify-center items-center gap-8 pt-20 bg-primary'>
+    <Container center>
       <h1 className="text-light text-5xl font-semibold"><span className="text-accent">Log</span>in</h1>
       <LoginForm />
-    </main>
+    </Container>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import { Container } from "@/src/components/layout";
 import Categorybar from "@/src/components/ui/Categorybar";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen min-h-screen flex flex-col items-center py-[105px] bg-primary">
+    <Container className="h-screen">
       <Categorybar />
       <div className="w-full h-full flex justify-center items-center">
         <img src='/logo.png' className="w-64 md:w-80 lg:w-[500px]" alt='' />
       </div>
-    </main>
+    </Container>
   );
 }
