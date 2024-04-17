cd22e25458778ab0492a4e2deec0bdbaf977fae0
GITZMBE <lucasfilip@hotmail.se>
2024-04-17 11:47:49 +0200

    Change from Image to img in main page displaying the logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
-
+import Categorybar from "@/src/components/ui/Categorybar";
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen flex justify-center items-center bg-primary">
-      <Image src='/logo.png' width='550' height='200' alt='' />
+    <main className="w-full h-screen min-h-screen flex flex-col items-center py-[105px] bg-primary">
+      <Categorybar />
+      <div className="w-full h-full flex justify-center items-center">
+        <img src='/logo.png' width='550' height='200' alt='' />
+      </div>
     </main>
   );
 }
