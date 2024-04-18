33fdc991623a48b01cc4bf4b1e6ab21febd258d9
GITZMBE <lucasfilip@hotmail.se>
2024-04-18 16:16:11 +0200

    Change padding from py to pt in Container component

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -9,7 +9,7 @@ interface ContainerProps extends ComponentPropsWithoutRef<"div"> {
 
 export const Container = ({ center, extraPadding, children, className, ...props }: ContainerProps) => {
   return (
-    <div className={`w-full min-h-screen flex flex-col gap-8 items-center ${ center && 'justify-center' } ${ extraPadding ? 'py-32' : 'py-[105px]' } px-4 sm:px-8 md:px-12 bg-primary ${ className }`} {...props}>
+    <div className={`w-full min-h-screen flex flex-col gap-8 items-center ${ center && 'justify-center' } ${ extraPadding ? 'pt-32' : 'pt-[105px]' } pb-8 px-4 sm:px-8 md:px-12 bg-primary ${ className }`} {...props}>
       { children }
     </div>
   )
