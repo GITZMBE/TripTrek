a5c00a3f18178d35e95267f520dd8f1bf8d5d8d4
GITZMBE <lucasfilip@hotmail.se>
2024-04-18 18:54:46 +0200

    Add functionality to the information step of the creation of a listing

--- src/components/listingSteps/InfoStep.tsx
+++ src/components/listingSteps/InfoStep.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { UseFormSetValue, UseFormUnregister, UseFormWatch } from 'react-hook-form';
+import { Counter } from './ui';
 
-export const InfoStep = () => {
+interface InfoStepProps {
+  unregister: UseFormUnregister<any>;
+  setValue: UseFormSetValue<any>;
+  watch: UseFormWatch<any>;
+  name: string;
+}
+
+export const InfoStep = ({ unregister, setValue, watch, name }: InfoStepProps) => {
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
+
   return (
-    <div>InfoStep</div>
+    <div className='max-w-[700px] min-h-[65vh]'>
+      <h1 className='text-light text-2xl text-center pb-4'>Write some information about your listing?</h1>
+      <div className='flex flex-col center gap-4 p-4 w-[700px] h-[60vh] shadow-inner overflow-y-auto scrollbar-hidden'>
+        <Counter title='Guests' subtitle='How many guests do you allow?' value={guestCount} onChange={(value) => setValue('guestCount', value)} />
+        <Counter title='Rooms' subtitle='How many rooms do you have?' value={roomCount} onChange={(value) => setValue('roomCount', value)} />
+        <Counter title='Bathrooms' subtitle='How many bathrooms do you have?' value={bathroomCount} onChange={(value) => setValue('bathroomCount', value)} />
+      </div>
+    </div>
   )
 }
 
