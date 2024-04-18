283542fb8146917aeb1e0b7858fefc698a2b0866
GITZMBE <lucasfilip@hotmail.se>
2024-04-18 18:55:25 +0200

    Modifying the Counter component

--- src/components/listingSteps/ui/Counter.tsx
+++ src/components/listingSteps/ui/Counter.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { AiOutlineMinus } from 'react-icons/ai';
+import React from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 interface CounterProps {
   title: string;
@@ -22,23 +22,28 @@ export const Counter = ({ title, subtitle, value, onChange }: CounterProps) => {
   };
 
   return (
-    <div className='flex flex-col gap-8'>
+    <div className='w-full flex justify-between gap-8'>
       <div className='flex flex-col'>
-        <div className='font-medium'>
-          <div className='font-light text-grey'>
-            { subtitle }
-          </div>          
+        <div className='text-light font-medium'>{title}</div>
+        <div className='font-light text-grey'>{subtitle}</div>
+      </div>
+      <div className='flex items-center gap-4'>
+        <div
+          onClick={onReduce}
+          className='w-10 h-10 rounded-full border-[1px] flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition'
+        >
+          <AiOutlineMinus />
         </div>
-        <div className='flex items-center gap-4'>
-          <div onClick={onReduce} className='w-10 h-10 rounded-full border-[1px] flex itmes-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition'>
-            <AiOutlineMinus size={16} />
-          </div>
-          <div className="font-light text-xl text-neutral-600">{ value }</div>
-          <button onClick={onAdd} className='w-10 h-10 rounded-full border-[1px] flex itmes-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition'>+</button>
+        <div className='font-light text-xl text-grey'>{value}</div>
+        <div
+          onClick={onAdd}
+          className='w-10 h-10 rounded-full border-[1px] flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition'
+        >
+          <AiOutlinePlus />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Counter;
