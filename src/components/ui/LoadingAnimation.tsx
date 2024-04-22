f20cc20754d1a5f40c71f3bbdde82834e52da441
GITZMBE <lucasfilip@hotmail.se>
2024-04-22 09:43:31 +0200

    Make some changes to some ui components

--- src/components/ui/LoadingAnimation.tsx
+++ src/components/ui/LoadingAnimation.tsx
@@ -2,13 +2,11 @@ import React, { ComponentPropsWithoutRef } from "react";
 
 interface LoadingAnimationProps extends ComponentPropsWithoutRef<"iframe"> {}
 
-export const LoadingAnimation = ({ width, height, className }: LoadingAnimationProps) => {
+export const LoadingAnimation = ({ ...props }: LoadingAnimationProps) => {
   return (
     <iframe
+      {...props}
       src='https://lottie.host/embed/f5d62494-ae89-4630-bb03-eadfc30f614a/1LvVjlu5hq.json'
-      width={width}
-      height={height}
-      className={className}
     ></iframe>
   );
 };
