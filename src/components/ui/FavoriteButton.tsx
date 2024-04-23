12355648bc9ef896b9f7298dab9cdfaca2c0ce21
GITZMBE <lucasfilip@hotmail.se>
2024-04-23 12:54:49 +0200

    Abstract heart icons/like buttons and functionality into external component 'FavoriteButton'

---
+++ src/components/ui/FavoriteButton.tsx
@@ -0,0 +1,65 @@
+import { loggedInUserState } from '@/src/recoil';
+import { Listing } from '@prisma/client';
+import React from 'react'
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { useRecoilState } from 'recoil';
+
+interface FavoriteButtonProps {
+  listing: Listing;
+};
+
+const FavoriteButton = ({ listing }: FavoriteButtonProps) => {
+  const [user_token, setUserToken] = useRecoilState(loggedInUserState);
+
+  const isFavorite = () => {
+    const favorites = user_token?.user.favoriteIds || [];
+    const includeUser = favorites.some((listingId: string) => listingId === listing.id);
+    return includeUser;
+  };
+
+  const isOwner = () => {
+    return listing.userId === user_token?.user.id;
+  }
+
+  const handleLikeListing = async () => {
+    const res = await fetch(process.env.NEXT_PUBLIC_BASEURL + `/api/users/${user_token?.user.id}/favorites`, { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ listingId: listing.id })
+    });
+    const updatedUser = await res.json();
+    setUserToken({user: updatedUser, token: user_token?.token || ""});
+  };
+
+  const handleRemoveFavorite = async () => {
+    const res = await fetch(process.env.NEXT_PUBLIC_BASEURL + `/api/users/${user_token?.user.id}/favorites`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ listingId: listing.id })
+    });
+    const updatedUser = await res.json();
+    setUserToken({user: updatedUser, token: user_token?.token || ""});
+  };
+  return (
+    <>
+      { isOwner() ? (
+        <span className='absolute top-4 right-4 text-light'>Owner</span>
+      ) : isFavorite() ? (
+        <div className='absolute top-4 right-4' onClick={e => {e.stopPropagation(); handleRemoveFavorite()}}>
+          <FaHeart className='text-love' />
+        </div>
+        
+      ) : (
+        <div className='absolute top-4 right-4' onClick={e => {e.stopPropagation(); handleLikeListing()}}>
+          <FaRegHeart className=' text-light' />
+        </div>
+      ) }    
+    </>
+  )
+}
+
+export default FavoriteButton
