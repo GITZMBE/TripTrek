5e2ce9958b91df3d20731d9f996d954287b0927e
GITZMBE <lucasfilip@hotmail.se>
2024-04-25 15:32:21 +0200

    Update on last commit

--- src/hooks/useErrorMessage.ts
+++ src/hooks/useErrorMessage.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const useErrorMessage = (defaultValue?: string) => {
+export const useErrorMessage = (defaultValue?: string | null) => {
   const [ errorMessage, setErrorMessage ] = useState<string | null>(defaultValue || null);
   return { errorMessage, setErrorMessage };
 };
