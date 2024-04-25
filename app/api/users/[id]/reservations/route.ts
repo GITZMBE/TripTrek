eca01e426f214673c3ad41635499bdec709a3416
GITZMBE <lucasfilip@hotmail.se>
2024-04-25 10:35:22 +0200

    Create endpoint to get all reservations a user has done

--- app/api/users/[id]/reservations/route.ts
+++ app/api/users/[id]/reservations/route.ts
@@ -1,6 +1,17 @@
 import prisma from "@/prisma";
 import { NextResponse } from "next/server";
 
+export const GET = async (req: Request, { params } : { params: { id: string } }) => {
+  const { id } = params;
+  const reservations = prisma.reservation.findMany({
+    where: {
+      userId: id
+    }
+  }) || [];
+
+  return NextResponse.json(reservations);
+};
+
 export const POST = async (req: Request) => {
   const body = await req.json();
   const reservation = await prisma.reservation.create({
