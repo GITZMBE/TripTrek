e565babda469e4f9862dca7408f733614b3c6956
GITZMBE <lucasfilip@hotmail.se>
2024-04-26 09:46:55 +0200

    Create component to render array of data and display message if empty and show if loading

--- src/components/listingSteps/ui/UploadImage.tsx
+++ src/components/listingSteps/ui/UploadImage.tsx
@@ -41,6 +41,8 @@ export const UploadImage = ({ unregister, setValue, watch, setError, clearErrors
     // if (savedImage.message) {
     //   return savedImage;
     // }
+    // setValue("image", savedImage.url);
+    // setIsLoading(false);
     setValue("image", "https://images.unsplash.com/photo-1520106212299-d99c443e4568?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     setIsLoading(false);    
   };
