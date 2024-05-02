f9c28df6e1a45ba866409249956e833b19f0d722
GITZMBE <lucasfilip@hotmail.se>
2024-05-02 10:14:10 +0200

    Change uri for clicking BookingCard and ReservationCard

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -9,7 +9,7 @@ interface ContainerProps extends ComponentPropsWithoutRef<"div"> {
 
 export const Container = ({ center, extraPadding, banner, children, className, ...props }: ContainerProps) => {
   return (
-    <div className={`w-full min-h-screen flex flex-col gap-8 items-center ${ center && 'justify-center' } ${ extraPadding ? 'pt-32' : 'pt-[105px]' } pb-8 ${ !banner && 'px-4 sm:px-8 md:px-12' } bg-primary ${ className }`} {...props}>
+    <div className={`w-full min-h-screen flex flex-col gap-8 items-center ${ center && 'justify-center' } ${ extraPadding ? 'pt-32' : 'pt-[105px]' } pb-8 ${ !banner && 'px-4 sm:px-8 md:px-12' } bg-primary ${ className } `} {...props}>
       { children }
     </div>
   )
