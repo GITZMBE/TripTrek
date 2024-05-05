c6d03feb3e999e13097e1e15dba63d01d1631934
GITZMBE <lucasfilip@hotmail.se>
2024-05-05 15:04:49 +0200

    Add visual design and transitions for chatroom

--- src/components/ui/ChatRecord.tsx
+++ src/components/ui/ChatRecord.tsx
@@ -1,22 +1,22 @@
 'use client';
 
 import { useCurrentUser } from '@/src/hooks';
-import { Chat, Listing, User } from '@prisma/client'
+import { Chat, Listing, Message, User } from '@prisma/client'
 import React, { Dispatch, SetStateAction } from 'react'
 import { MdVerified } from 'react-icons/md';
 
 interface IProps {
-  chat: Chat;
+  chat: Chat & { listing: Listing, members: User[], owner: User };
+  currentChat: Chat & { listing: Listing, owner: User, members: User[], messages: Message[] } | null;
   setCurrentChat: Dispatch<SetStateAction<any>>;
 }
 
-export const ChatRecord = ({ chat, setCurrentChat }: IProps) => {
+export const ChatRecord = ({ chat, currentChat, setCurrentChat }: IProps) => {
   const { currentUser: user } = useCurrentUser();
 
   return (
-    <button className='w-full flex justify-between gap-2 py-2 px-4 border-b-2 border-b-secondary bg-transparent hover:bg-secondary opacity-50 hover:opacity-100 transition' onClick={() => setCurrentChat(chat)}>
-      {/* <p>{ chat.title ? chat.title : `${chat.ownerId}'s chat${ chat.listingId && ` for ${ chat.listing.title }` }` }</p> */}
-      <p className='text-grey overflow-hidden text-ellipsis text-nowrap'>{ chat.title ? chat.title : `${chat.ownerId}'s chat` }</p>
+    <button className={`w-full flex justify-between gap-2 py-2 px-4 border-b-2 border-b-secondary ${ chat?.id === currentChat?.id ? 'bg-secondary opacity-100': 'bg-transparent opacity-50' } hover:bg-secondary hover:opacity-100 transition`} onClick={() => setCurrentChat(chat)}>
+      <p className='text-grey overflow-hidden text-ellipsis text-nowrap'>{ chat.title ? chat.title : chat.owner ? `${chat.owner.name}'s chat` : '' }</p>
       { user?.id === chat.ownerId && (
         <MdVerified size={24} className='text-verified' />
       )}
--- src/components/ui/TextMessage.tsx
+++ src/components/ui/TextMessage.tsx
@@ -11,11 +11,11 @@ interface IProps {
 const TextMessage = ({ message }: IProps) => {
   const { currentUser: user } = useCurrentUser();
 
-  return (
+  return user && (
     <div className={`w-full flex ${ message.userId === user?.id ? 'justify-end' : 'justify-start' }`}>
       <p className='max-w-[60%] text-grey text-wrap text-left bg-secondary p-2 rounded-lg'>{ message.text }</p>
     </div>
   )
 }
 
-export default TextMessage
+export default TextMessage;
