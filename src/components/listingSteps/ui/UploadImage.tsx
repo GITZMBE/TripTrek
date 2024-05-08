aafaa1278c88151641c4587c7d25d10a5255dd86
GITZMBE <lucasfilip@hotmail.se>
2024-05-08 16:15:31 +0200

    Made the slideshow link to different pages depending on if you are the owner of the listing

--- src/components/listingSteps/ui/UploadImage.tsx
+++ src/components/listingSteps/ui/UploadImage.tsx
@@ -27,22 +27,22 @@ export const UploadImage = ({ unregister, setValue, watch, setError, clearErrors
   const uploadImage = async (file: File) => {
     try {
       setIsLoading(true);
-      const imageData = await fetch(
-        process.env.NEXT_PUBLIC_BASEURL + "/api/images/upload",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": file.type || "image/*",
-            "accept": "image/*"
-          },
-          body: file,
-        }
-      );
-      const savedImage = await imageData.json();
-      if (savedImage.message) {
-        return savedImage;
-      }
-      setValue("image", savedImage.url);
+      // const imageData = await fetch(
+      //   process.env.NEXT_PUBLIC_BASEURL + "/api/images/upload",
+      //   {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": file.type || "image/*",
+      //       "accept": "image/*"
+      //     },
+      //     body: file,
+      //   }
+      // );
+      // const savedImage = await imageData.json();
+      // if (savedImage.message) {
+      //   return savedImage;
+      // }
+      setValue("image", 'https://images.unsplash.com/photo-1594069758873-e79e9075eb7d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
     } finally {
       setIsLoading(false);
     }
