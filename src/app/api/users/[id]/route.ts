d6e2f1dea7b33809ad8f80b3c147b0b9b4d632fd
GITZMBE <lucasfilip@hotmail.se>
2024-05-08 14:26:59 +0200

    route prettier

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -3,9 +3,11 @@ import { NextResponse } from "next/server";
 
 export const GET = async (req: Request, { params }: { params: { id: string } }) => {
   const { id } = params;
-  const user = await prisma.user.findUnique({ where: { id } });
-
-  if (!user) return null;
+  const user = await prisma.user.findUnique({ 
+    where: { 
+      id 
+    } 
+  });
 
   return NextResponse.json(user);
 };
