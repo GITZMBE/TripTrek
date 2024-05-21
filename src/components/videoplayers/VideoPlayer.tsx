5d55cb9cf54f0e1f1f46b4284bed3f46f75a0fee
GITZMBE <lucasfilip@hotmail.se>
2024-05-21 13:10:00 +0200

    Implemented a commercial

---
+++ src/components/videoplayers/VideoPlayer.tsx
@@ -0,0 +1,16 @@
+import React, { ComponentPropsWithoutRef } from 'react';
+
+interface IProps extends ComponentPropsWithoutRef<"video"> {
+  src: string;
+  type?: string;
+}
+
+export const VideoPlayer = ({ src, type, ...props }: IProps) => {
+  return (
+    <video { ...props }>
+      <source src={src} type={ type || "video/mp4" } />
+    </video>
+  )
+}
+
+export default VideoPlayer;
