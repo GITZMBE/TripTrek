7132edd9050be7d23101a7f8b15e2f20da6e25cb
GITZMBE <lucasfilip@hotmail.se>
2024-05-22 13:29:08 +0200

    Comment out SplineScene.tsx

--- src/components/layout/SplineScene.tsx
+++ src/components/layout/SplineScene.tsx
@@ -1,14 +1,14 @@
 "use client";
 // https://app.spline.design/file/49135fdc-18b5-40cc-a9be-8a71de8150ef
 
-import React, { useEffect, useRef } from "react";
-import { Application } from '@splinetool/runtime';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import SplineLoader from '@splinetool/loader';
+// import React, { useEffect, useRef } from "react";
+// import { Application } from '@splinetool/runtime';
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import SplineLoader from '@splinetool/loader';
 
 export const SplineScene = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  // const canvasRef = useRef<HTMLCanvasElement>(null);
 
   // useEffect(() => {
   //   if (!canvasRef.current) return;
@@ -32,58 +32,58 @@ export const SplineScene = () => {
   // }, []);
 
   // camera
-  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 10, 10000);
-  camera.position.set(-323.8, 682.67, 735.02);
-  camera.quaternion.setFromEuler(new THREE.Euler(-0.77, -0.01, -0.01));
+  // const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 10, 10000);
+  // camera.position.set(-323.8, 682.67, 735.02);
+  // camera.quaternion.setFromEuler(new THREE.Euler(-0.77, -0.01, -0.01));
 
   // scene
-  const scene = new THREE.Scene();
+  // const scene = new THREE.Scene();
 
   // spline scene
-  const loader = new SplineLoader();
-  loader.load(
-    'https://prod.spline.design/3hTFcoEPy7XXr9It/scene.splinecode',
-    (splineScene) => {
-      scene.add(splineScene);
-    }
-  );
+  // const loader = new SplineLoader();
+  // loader.load(
+  //   'https://prod.spline.design/3hTFcoEPy7XXr9It/scene.splinecode',
+  //   (splineScene) => {
+  //     scene.add(splineScene);
+  //   }
+  // );
 
   // renderer
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setAnimationLoop(animate);
-  document.body.appendChild(renderer.domElement);
+  // const renderer = new THREE.WebGLRenderer({ antialias: true });
+  // renderer.setSize(window.innerWidth, window.innerHeight);
+  // renderer.setAnimationLoop(animate);
+  // document.body.appendChild(renderer.domElement);
 
   // scene settings
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFShadowMap;
+  // renderer.shadowMap.enabled = true;
+  // renderer.shadowMap.type = THREE.PCFShadowMap;
 
-  scene.background = new THREE.Color('#72cc4f');
-  renderer.setClearAlpha(1);
+  // scene.background = new THREE.Color('#72cc4f');
+  // renderer.setClearAlpha(1);
 
   // orbit controls
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.125;
+  // const controls = new OrbitControls(camera, renderer.domElement);
+  // controls.enableDamping = true;
+  // controls.dampingFactor = 0.125;
 
-  window.addEventListener('resize', onWindowResize);
-  function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+  // window.addEventListener('resize', onWindowResize);
+  // function onWindowResize() {
+  //   camera.aspect = window.innerWidth / window.innerHeight;
+  //   camera.updateProjectionMatrix();
+  //   renderer.setSize(window.innerWidth, window.innerHeight);
+  // }
 
-  function animate() {
-    controls.update();
-    renderer.render(scene, camera);
-  }
+  // function animate() {
+  //   controls.update();
+  //   renderer.render(scene, camera);
+  // }
 
 
   return (
     <div className='w-full h-[80vh]'>
       {/* <script type="module" src="https://unpkg.com/@splinetool/viewer@1.3.7/build/spline-viewer.js"></script>
       <spline-viewer url="https://prod.spline.design/3hTFcoEPy7XXr9It/scene.splinecode"></spline-viewer> */}
-      <canvas ref={canvasRef} className='w-full'></canvas>
+      {/* <canvas ref={canvasRef} className='w-full'></canvas> */}
     </div>
   );
 };
