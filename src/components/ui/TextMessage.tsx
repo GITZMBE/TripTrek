ba5c555e2aeb900960755829207acc820d37ff26
GITZMBE <lucasfilip@hotmail.se>
2024-05-28 14:31:22 +0200

    Show how long time ago a message was sent instead of the user when hovering

--- src/components/ui/TextMessage.tsx
+++ src/components/ui/TextMessage.tsx
@@ -2,6 +2,7 @@
 
 import { useCurrentUser } from '@/src/hooks';
 import { ExtendedMessage } from '@/src/models/ExtendedMessage';
+import { format, formatDistance } from 'date-fns';
 import React from 'react'
 
 interface IProps {
@@ -13,8 +14,10 @@ const TextMessage = ({ message }: IProps) => {
 
   return user && (
     <div className={`group w-full flex flex-col gap-1 ${ message.userId === user?.id ? 'items-end' : 'items-start' }`}>
-      <p className='max-w-[60%] text-light text-wrap text-left bg-secondary p-2 rounded-lg'>{ message.text }</p>
-      <p className='max-w-[60%] text-grey text-no-wrap text-right text-sm opacity-0 group-hover:opacity-100 transition'>{ message.userId === user?.id ? 'You' : message.user.name }</p>
+      <p className='sibling-hover max-w-[60%] text-light text-wrap text-left bg-secondary p-2 rounded-lg'>{ message.text }</p>
+      <p className='sibling max-w-[60%] text-grey text-no-wrap text-right text-sm h-0 opacity-0 transition-size'>
+        { formatDistance(message.createdAt, new Date(), { addSuffix: true }) }
+      </p>
     </div>
   )
 }
